8c877a279674b96fd9911f7b9f60e3f6082a0d17
Nicola Bolton <nicola.bolton@yorkandchapel.com>
2020-06-04 15:48:05 -0700

    install storybook and create Button stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,17 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ["../src/**/*.stories.(js|mdx)"],
   addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-  ],
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-contexts/register",
+    "@storybook/addon-backgrounds/register",
+    "@storybook/addon-knobs",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true
+      }
+    }
+  ]
 };
